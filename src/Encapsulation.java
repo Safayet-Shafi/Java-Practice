557c1d467f51715dc3405eba0f43de6391fdd48c
Safayet-Shafi <77004144+Safayet-Shafi@users.noreply.github.com>
2026-04-21 10:55:33 +0600

    test

---
+++ src/Encapsulation.java
@@ -0,0 +1,13 @@
+public class Encapsulation {
+    private String name;
+    private int age;
+
+    //getter
+    public String  getName(){
+        return name;
+    }
+    //setter
+    public void setName(String name){
+        this.name=name;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,20 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-            int [][] matrix ={
-                    {1,2,3,1},
-                    {4,5,6,1},
-                    {7,8,9,1},
-            };
-        System.out.println("matrix.length = " + matrix.length); // row
-        System.out.println("matrix[0].length = " + matrix[0].length); //col
-        Scanner sc =new Scanner(System.in);
-        int num =sc.nextInt();
-        System.out.println("num = " + num);
+//            int [][] matrix ={
+//                    {1,2,3,1},
+//                    {4,5,6,1},
+//                    {7,8,9,1},
+//            };
+//        System.out.println("matrix.length = " + matrix.length); // row
+//        System.out.println("matrix[0].length = " + matrix[0].length); //col
+//        Scanner sc =new Scanner(System.in);
+//        int num =sc.nextInt();
+//        System.out.println("num = " + num);
+        Encapsulation enc = new Encapsulation();
+        enc.setName("Safayet");
+        System.out.println("enc.getName() = " + enc.getName());
+        
+        
     }
 }
